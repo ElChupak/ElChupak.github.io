1cf194478009b76b8792b8f23d18a73440e607b0
Dmytro <113373722+ElChupak@users.noreply.github.com>
2024-06-15 21:45:05 +0200

    Add files via upload

---
+++ kyivjobs/js/translationArr.js
@@ -0,0 +1,264 @@
+const langArr = {
+    // "fav-title" :  {
+    //     "en": "Kyiv Jobs",
+    //     "ua": "Kyiv Jobs",
+    //     "nl": "Kyiv Jobs",
+    // }, 
+    "clients" :  {
+        "en": "Clients",
+        "ua": "Клієнти ",
+        "nl": "Klanten",
+    }, 
+    "offers" :  {
+        "en": "Offers",
+        "ua": "Пропозиції",
+        "nl": "Vacatures",
+    },
+    "contacts" :  {
+        "en": "Contacts",
+        "ua": "Контакти",
+        "nl": "Contacten",
+    },
+    "first-slide-m" :  {
+        "en": "We are from the Netherlands created to help Ukrainians to find a job.",
+        "ua": "Ми з Нідерландів, створені, щоб допомогти українцям знайти роботу.",
+        "nl": "Wij zijn vanuit Nederland opgericht om Oekraïners te helpen aan een baan.",
+    },
+    "second-slide-m" :  {
+        "en": "More than 2 years of cooperation with different companies in the Nederlands",
+        "ua": "Більше 2 років співпраці з різними компаніями в Нідерландах",
+        "nl": "Ruim 2 jaar samenwerking met verschillende bedrijven in Nederland",
+    },
+    "third-slide-m" :  {
+        "en": "We’ve already helped to 300+ Ukrainians for the last two years.",
+        "ua": "За останні два роки ми вже допомогли понад 300 українцям.",
+        "nl": "We hebben de afgelopen twee jaar al meer dan 300 Oekraïners geholpen.",
+    },
+    "manage-sup" :  {
+        "en": "We’ll help find your business perfect worker",
+        "ua": "Ми допоможемо знайти для вашого бізнесу ідеального працівника",
+        "nl": "Wij helpen u de perfecte werknemer voor uw bedrijf te vinden",
+    },
+    "manage-sub" :  {
+        "en": "Our company will find right person for your business. We’ve been working with different kinds of companies and ready for any new cooperation. ",
+        "ua": "Наша компанія знайде людину для вашого бізнесу. Ми працюємо з різними компаніями і готові до будь-якої співпраці.",
+        "nl": "Ons bedrijf vindt de juiste persoon voor uw bedrijf. We hebben met verschillende soorten bedrijven samengewerkt en zijn klaar voor elke nieuwe samenwerking.",
+    },
+    "manage-log" :  {
+        "en": "LOGISTICS",
+        "ua": "ЛОГІСТИКА",
+        "nl": "LOGISTIEK",
+    },
+    "manage-clean" :  {
+        "en": "CLEANING",
+        "ua": "ПРИБИРАННЯ",
+        "nl": "SCHOONMAAK",
+    },
+    "manage-hosp" :  {
+        "en": "HOSPITALITY",
+        "ua": "ГОСТИННІСТЬ",
+        "nl": "GASTVRIJHEID",
+    },
+    // Clients
+
+
+                                        // OFFERS
+
+    "offers-sup" :  {
+        "en": "We offer now!",
+        "ua": "Ми пропонуємо зараз!",
+        "nl": "Wij bieden nu!",
+    },
+    "offers-sub" :  {
+        "en": "Below you can see all the vacancies and information that we provide for our people.",
+        "ua": "Нижче ви можете переглянути всі вакансії та інформацію, яку ми надаємо нашим співробітникам.",
+        "nl": "Hieronder ziet u alle vacatures en informatie die wij voor onze mensen verzorgen.",
+    },
+
+    //Cleaning   
+
+    "offers-clean" :  {
+        "en": "Cleaning",
+        "ua": "Прибирання ",
+        "nl": "Schoonmaak",
+    }, 
+    "posit-clean-descr" :  {
+        "en": "What are you going to do?",
+        "ua": "Що ти збираєшся робити?",
+        "nl": "Wat ga je doen?",
+    },
+    "link-clean-more " :  {
+        "en": "MORE INFO",
+        "ua": "БІЛЬШЕ ІНФО ",
+        "nl": "MEER INFO",
+    },
+    "clean-list-top" :  {
+        "en": "Responsibilities:",
+        "ua": "Обов'язки: ",
+        "nl": "Verantwoordelijkheden:",
+    }, 
+    "list-clean-less " :  {
+        "en": "LESS INFO",
+        "ua": "МЕНШЕ ІНФО ",
+        "nl": "MINDER INFO",
+    }, 
+    "list-clean-1" :  {
+        "en": "CliEnsure all surfaces are free of dust and dirt.nts",
+        "ua": "Переконайтеся, що всі поверхні очищені від пилу та бруду. ",
+        "nl": "Zorg ervoor dat alle oppervlakken stof- en vuilvrij zijn.",
+    }, 
+    "list-clean-2" :  {
+        "en": "Maintain clean and hygienic flooring.",
+        "ua": "Підтримуйте чисте та гігієнічне покриття підлоги. ",
+        "nl": "Zorg voor een schone en hygiënische vloer.",
+    }, 
+    "list-clean-3" :  {
+        "en": "Keep carpets clean and free of debris.",
+        "ua": "Підтримувати килими чистими та вільними від сміття. ",
+        "nl": "Houd tapijten schoon en vrij van vuil.",
+    }, 
+    "list-clean-4" :  {
+        "en": "Collect and dispose of waste materials.",
+        "ua": "Збирайте та утилізуйте відходи. ",
+        "nl": "Verzamel en voer afvalmaterialen af.",
+    }, 
+    "list-clean-5" :  {
+        "en": "Sanitize toilets, sinks, and other restroom fixtures.",
+        "ua": "Продезінфікуйте унітази, раковини та інше обладнання в туалетах. ",
+        "nl": "Ontsmet toiletten, gootstenen en andere toiletvoorzieningen.",
+    }, 
+    "list-clean-6" :  {
+        "en": "Replenish cleaning and restroom supplies.",
+        "ua": "Поповніть засоби для прибирання та туалету. ",
+        "nl": "Aanvullen van schoonmaak- en toiletbenodigdheden.",
+    }, 
+
+    //Warehouse
+
+    "offers-ware" :  {
+        "en": "Warehouse",
+        "ua": "Склад ",
+        "nl": "Magazijn",
+    },
+    "posit-ware-descr" :  {
+        "en": "What are you going to do?",
+        "ua": "Що ти збираєшся робити?",
+        "nl": "Wat ga je doen?",
+    },
+    "link-ware-more " :  {
+        "en": "MORE INFO",
+        "ua": "БІЛЬШЕ ІНФО ",
+        "nl": "MEER INFO",
+    },
+    "list-ware-top" :  {
+        "en": "The work of an order picker in logistics consists of:",
+        "ua": "Робота комплектувальника в логістиці складається з: ",
+        "nl": "De werkzaamheden van een orderpicker in de logistiek bestaan ​​uit:",
+    },
+    "list-ware-1" :  {
+        "en": " Picking and packing orders;",
+        "ua": "Комплектація та пакування замовлень; ",
+        "nl": "Orderpicken en inpakken;",
+    },
+    "list-ware-2" :  {
+        "en": "Store incoming goods in the warehouse;",
+        "ua": "Зберігати товар, що надходить, на складі; ",
+        "nl": "Het opslaan van binnenkomende goederen in het magazijn;",
+    }, 
+    "list-ware-3" :  {
+        "en": " Check outgoing goods (prepare for shipment);",
+        "ua": "Перевірити вихідні товари (підготувати до відправлення); ",
+        "nl": "Uitgaande goederen controleren (verzendklaar maken);",
+    }, 
+    "list-ware-4" :  {
+        "en": "Repackaging of items.",
+        "ua": "Перепакування товарів. ",
+        "nl": "Herverpakken van goederen.",
+    },
+    "list-ware-less " :  {
+        "en": "LESS INFO",
+        "ua": "МЕНШЕ ІНФО ",
+        "nl": "MINDER INFO",
+    }, 
+
+
+                            // CONTACT
+    "cont-sup" :  {
+        "en": "Contact us!",
+        "ua": "Зв'яжіться з нами! ",
+        "nl": "Contact met ons op!",
+    },
+    "cont-name" :  {
+        "en": "Name:",
+        "ua": "Ім'я:",
+        "nl": "Naam:",
+    },
+    "cont-phone" :  {
+        "en": "Phone:",
+        "ua": "Телефон:",
+        "nl": "Telefoon:",
+    },
+    "cont-mess" :  {
+        "en": "Message:",
+        "ua": "Повідомлення:",
+        "nl": "Bericht:",
+    },
+    "cont-span" :  {
+        "en": "I accept the",
+        "ua": "Я приймаю",
+        "nl": "Ik accepteer de", //gebruiksvoorwaarden
+    },
+    "cont-link" :  {
+        "en": "Terms of Service",
+        "ua": "Загальні положення та умови",
+        "nl": "Gebruiksvoorwaarden",
+    },
+    "cont-send" :  {
+        "en": "Send",
+        "ua": "Надіслати",
+        "nl": "Versturen",
+    },
+
+                                // BASEMENT
+
+    
+    "base-rout" :  {
+        "en": "Click to find a rout",
+        "ua": "Натисніть, щоб знайти маршрут",
+        "nl": "Klik om een ​​route te vinden",
+    },
+    "base-mail" :  {
+        "en": "Click to copy mail",
+        "ua": "Натисніть, щоб скопіювати email",
+        "nl": "Klik om e-mail te kopiëren",
+    },
+    "base-copied" :  {
+        "en": "Email copied",
+        "ua": "Пошту скопійовано",
+        "nl": "E-mail gekopieerd",
+    }, 
+    "base-sched" :  {
+        "en": "Working hours:",
+        "ua": "Робочі години: ",
+        "nl": "Werkuren:",
+    }, 
+    "base-days" :  {
+        "en": "MONDAY TO FRIDAY : <br> 9 AM - 5 PM",
+        "ua": "Понеділок - П`ятниця : <br> 9:00 - 17:00 ",
+        "nl": "MAANDAG TOT VRIJDAG: <br> 9.00 - 17.00 uur",
+    }, 
+    "base-cont-1" :  {
+        "en": "Rami  Azab",
+        "ua": "Рамі Азаб",
+        "nl": "Rami  Azab",
+    }, 
+    "base-cont-2" :  {
+        "en": "Mark  Joosten",
+        "ua": "Марк Йостен ",
+        "nl": "Mark  Joosten",
+    }, 
+    
+    
+    
+                            
+}
